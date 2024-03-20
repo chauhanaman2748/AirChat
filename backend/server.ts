c472ee5863b5d5d29c40296a12bdbfe237f8675a
My Name <spyman2748@gmail.com>
2024-03-20 22:32:20 +0530

    Deployment

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import path from "path";
+import express from 'express';
 import dotenv from 'dotenv';
 import { authRoutes, messageRoutes, userRoutes } from "./routes";
 import { connectMongoDB } from "./db";
@@ -6,6 +7,8 @@ import cookieParser from "cookie-parser";
 import { app, server } from './services';
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -15,9 +18,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, World!");
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
